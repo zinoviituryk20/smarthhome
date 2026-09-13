e2dc5ae42cc86a953a0da5d01ef309095dac431c
zinovii <zinovii.turyk@gmail.com>
2026-09-13 21:01:15 +0300

    feat: add devices/locations repository
    -Add Location Repository, DbEntities, Domain Models
    -Add Device Repository, DbEntities, Domain Models

---
+++ SmartHome.API/src/SmartHome.DeviceAPI/Controllers/LocationsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartHome.DeviceAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class LocationsController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        return Ok();
+    }
+
+    [HttpGet("id")]
+    public IActionResult Get(string id)
+    {
+        return Ok();
+    }
+
+    [HttpPost]
+    public IActionResult Create()
+    {
+        return Ok();
+    }
+
+    [HttpDelete("id")]
+    public IActionResult Delete(string id)
+    {
+        return Ok();
+    }
+}
